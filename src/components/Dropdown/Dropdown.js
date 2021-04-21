f1a7b5631a77ebca1e8588496e261aa6df8694bb
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-04-21 15:16:17 +0300

    dropdown using useState to manage its data

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './dropdown.css'
 
 const Dropdown = props => {
+
+    const [selectedDropValue, setSelectDropValue] = useState('')
+
+    const handleOnChange = (e) => {
+        setSelectDropValue(e.target.value)
+    }
+
     return (
         <div className="container">
-            <select>
+            <select value={selectedDropValue} onChange={handleOnChange}>
                {props.options.map((item, index) => 
                <option key={index} value={item.value}>
                    {item.name}
                </option>)}
             </select>
+            <p>{selectedDropValue}</p>
         </div>
     )
 }
