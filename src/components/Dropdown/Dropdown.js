2b9daba3835c672f8eba62d50258904b5532151a
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-04-22 14:59:15 +0300

    listbox item and bug fix on Dropdown component

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -14,16 +14,14 @@ const Dropdown = props => {
     }
 
     return (
-        <div className="container">
-            <select value={props.selectedValue} onChange={dropdownChanged}>
-               {props.options.map((item, idx) => 
-               <option key={idx} value={item.id}>
-                   {item.name}
-               </option>)}
-            </select>
-            {/* <p>{props.selectedDropValue}</p> */}
+        <div className="col-sm-6 form-group row px-0">     
+            <label className="form-label col-sm-2">{props.label}</label>       
+            <select value={props.selectedValue} onChange={dropdownChanged} className="form-control form-control-sm col-sm-10">
+                <option key={0}>Select...</option>
+                {props.options.map((item, idx) => <option key={idx + 1} value={item.id}>{item.name}</option>)}
+            </select>            
         </div>
-    )
+    );
 }
 
 export default Dropdown
